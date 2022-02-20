3a5dbc563be7e4c9ccfd7ed5f3ae2bcb2f755f8c
mtbr39 <nm1552jp@gmail.com>
2022-02-20 19:10:11 +0900

    article

--- src/components/blog-title-item.js
+++ src/components/blog-title-item.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Img from 'gatsby-image'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
-import styles from './blog-title-item.module.css'
+import * as styles from './blog-title-item.module.css'
 
 export default ({ article }) => (
-  <div className={styles.preview}>
-    
+  <div className={styles.box}>
     <div>
-      <h3 className={styles.previewTitle}>
+      <h3 className={styles.title}>
         <Link to={`/blog/${article.slug}`}>{article.title}</Link>
       </h3>
     </div>
-    {article.tags &&
-      article.tags.map((tag) => (
-        <p className={styles.tag} key={tag}>
-          {tag}
-        </p>
-      ))}
-    <small>{article.publishDate}</small>
-
+    <div>
+      {article.tags &&
+        article.tags.map((tag) => (
+          <small className={styles.tag} key={tag}>
+            {tag}
+          </small>
+        ))}
+      <small className={styles.date}>{article.publishDate}</small>
+    </div>
   </div>
 )
