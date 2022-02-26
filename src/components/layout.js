8724873007a88e9010e36b87a9ab2bba6bca1eb8
mtbr39 <nm1552jp@gmail.com>
2022-02-26 20:20:28 +0900

    layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,9 +10,9 @@ class Template extends React.Component {
     let header
 
     let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+    // if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+    //   rootPath = __PATH_PREFIX__ + `/`
+    // }
 
     return (
       <Container>
