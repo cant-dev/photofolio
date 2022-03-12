d4ac53d0498f507cbee07ed2bed7c9347c22032a
mtbr39 <nm1552jp@gmail.com>
2022-03-12 23:30:59 +0900

    add film-post

---
+++ src/templates/film-post.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import { Helmet } from 'react-helmet'
+import get from 'lodash/get'
+import Layout from '../components/layout'
+
+import * as heroStyles from '../components/hero.module.css'
+import * as styles from './film-post.module.css'
+
+class FilmPostTemplate extends React.Component {
+  render() {
+    const post = get(this.props, 'data.contentfulBlogPost')
+    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+
+    return (
+      <Layout location={this.props.location}>
+        <div style={{ background: '#fff' }}>
+          <Helmet title={`${post.title} | ${siteTitle}`} />
+          <div className={heroStyles.hero}></div>
+          <div className="wrapper">
+            <h1 className="section-headline">{post.title}film</h1>
+            <p
+              style={{
+                display: 'block',
+              }}
+            >
+              {post.publishDate}
+            </p>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: post.body.childMarkdownRemark.html,
+              }}
+            />
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default FilmPostTemplate
+
+export const pageQuery = graphql`
+  query FilmPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishDate(formatString: "MMMM Do, YYYY")
+      heroImage {
+        fluid(maxWidth: 1180, background: "rgb:000000") {
+          ...GatsbyContentfulFluid_tracedSVG
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
