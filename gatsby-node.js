ebe6404469ae7366089b9d8a9b7f8ee6b44986d8
mtbr39 <nm1552jp@gmail.com>
2022-03-14 20:31:34 +0900

    add film-post

--- gatsby-node.js
+++ gatsby-node.js
@@ -50,4 +50,52 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       })
     })
   }
+
+  // Define a template for film post
+  const filmPost = path.resolve('./src/templates/film-post.js')
+
+  const filmResult = await graphql(
+    `
+      {
+        allContentfulBlogPost {
+          nodes {
+            title
+            slug
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful posts`,
+      result.errors
+    )
+    return
+  }
+
+  const filmPosts = filmResult.data.allContentfulBlogPost.nodes
+
+  // Create blog posts pages
+  // But only if there's at least one blog post found in Contentful
+  // `context` is available in the template as a prop and as a variable in GraphQL
+
+  if (filmPosts.length > 0) {
+    filmPosts.forEach((post, index) => {
+      const previousPostSlug = index === 0 ? null : filmPosts[index - 1].slug
+      const nextPostSlug =
+        index === filmPosts.length - 1 ? null : filmPosts[index + 1].slug
+
+      createPage({
+        path: `/film/${post.slug}/`,
+        component: filmPost,
+        context: {
+          slug: post.slug,
+          previousPostSlug,
+          nextPostSlug,
+        },
+      })
+    })
+  }
 }
