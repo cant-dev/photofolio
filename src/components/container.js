512632935aa5fc735841c278443163b282488793
mtbr39 <matsubara@hakobune.co.jp>
2022-05-15 19:35:03 +0900

    TOP button

--- src/components/container.js
+++ src/components/container.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default ({ children }) => (
   <div class="background-pattern-wrapper" style={{ margin: '0 auto' }}>
-    <div class="pattern-film"></div>
-    <div>{children}</div>
+    <div class="pattern-film">
+      <div class="top-button-block">
+        <div class="up-forward-mark"></div>
+        <div class="top-text">TOP</div>
+      </div>
+    </div>
+    <div class="between-pattern">{children}</div>
     <div class="pattern-film"></div>
   </div>
 )
