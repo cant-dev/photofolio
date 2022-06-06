4f0f74bbd3d9a8431f0a5a1e4861c82b01d1dafb
mtbr39 <nm1552jp@gmail.com>
2022-06-06 22:34:35 +0900

    container : top link

--- src/components/container.js
+++ src/components/container.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import * as styles from './container.module.css'
+import { Link } from 'gatsby'
 
 export default ({ children }) => (
   <div className={styles.backgroundPatternWrapper} style={{ margin: '0 auto' }}>
     <div className={styles.patternFilm}>
-      <div className={styles.topButtonBlock}>
-        <div className={styles.upForwardMark}></div>
-        <div className={styles.topText}>TOP</div>
-      </div>
+      <Link to={`/`}>
+        <div className={styles.topButtonBlock}>
+          <div className={styles.upForwardMark}></div>
+          <div className={styles.topText}>TOP</div>
+        </div>
+      </Link>
     </div>
     <div className={styles.betweenPattern}>{children}</div>
     <div className={styles.patternFilm}></div>
