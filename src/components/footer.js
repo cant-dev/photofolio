f62393049e02ec8b6ddd073317a0f140a3693eb5
mtbr39 <nm1552jp@gmail.com>
2022-07-17 21:44:14 +0900

    footer : sns link

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,15 @@ import * as styles from './footer.module.css'
 export default () => (
   <ul className={styles.footer}>
     <li className={styles.item}>
-      <Link to="/film">Twitter</Link>
+      <a href="https://twitter.com/only_dog_win" target="_blank">
+        Twitter
+      </a>
     </li>
     <li className={styles.partition}></li>
     <li className={styles.item}>
-      <Link to="/me/">Instagram</Link>
+      <a href="https://www.instagram.com/ono.yah/?hl=ja" target="_blank">
+        Instagram
+      </a>
     </li>
   </ul>
 )
